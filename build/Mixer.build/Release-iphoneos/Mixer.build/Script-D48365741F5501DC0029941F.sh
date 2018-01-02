12fda278679e3a372a2cfa02245eba0ae18e4d73
jacob-abraham <jacob.r.abraham@gmail.com>
2018-01-02 16:32:06 -0700

    finished gitignore addition

--- build/Mixer.build/Release-iphoneos/Mixer.build/Script-D48365741F5501DC0029941F.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-TAGS="FEAT:"
-find "${SRCROOT}/Mixer" \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
